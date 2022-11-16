678cc555bf70f71848b2b0adaf738b173544cc6c
Amit Agarwal <1989amit@gmail.com>
2022-11-16 17:59:57 +0530

    Initial Update

---
+++ main.tf
@@ -0,0 +1,192 @@
+################################################################################
+# VPC
+################################################################################
+
+resource "aws_vpc" "myvpc" {
+  count = var.create_vpc ? 1 : 0
+
+  cidr_block          = var.use_ipam_pool ? null : var.cidr
+  ipv4_ipam_pool_id   = var.ipv4_ipam_pool_id
+  ipv4_netmask_length = var.ipv4_netmask_length
+
+  assign_generated_ipv6_cidr_block = var.enable_ipv6
+  ipv6_cidr_block                  = var.ipv6_cidr
+  ipv6_ipam_pool_id                = var.ipv6_ipam_pool_id
+  ipv6_netmask_length              = var.ipv6_netmask_length
+
+  instance_tenancy               = var.instance_tenancy
+  enable_dns_hostnames           = var.enable_dns_hostnames
+  enable_dns_support             = var.enable_dns_support
+  enable_classiclink             = null 
+  enable_classiclink_dns_support = null
+
+  #tags = merge(
+  #  { "Name" = var.name },
+  #  var.tags,
+  #  var.vpc_tags,
+  #)
+}
+
+################################################################################
+# MyPublic Subnet
+################################################################################
+
+resource "aws_subnet" "MyPublicSubnet" {
+  count = var.create_vpc ? length(var.public_subnets) : 0
+  vpc_id     = aws_vpc.myvpc[0].id
+  cidr_block = element(var.public_subnets,count.index)
+  availability_zone               = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) > 0 ? element(var.azs, count.index) : null
+  map_public_ip_on_launch         = var.map_public_ip_on_launch
+  assign_ipv6_address_on_creation = var.assign_ipv6_address_on_creation
+  ipv6_cidr_block = var.enable_ipv6 && length(var.public_subnet_ipv6_prefixes) > 0 ? cidrsubnet(aws_vpc.myvpc[0].ipv6_cidr_block, 8, var.public_subnet_ipv6_prefixes[count.index]) : null
+  tags = {
+    Name = "Public-Subnet-${ element(var.azs, count.index)}"
+  }
+}
+
+################################################################################
+# MyPrivate Subnet
+################################################################################
+
+resource "aws_subnet" "MyPrivateSubnet" {
+  count = var.create_vpc ? length(var.private_subnets) : 0
+  vpc_id     = aws_vpc.myvpc[0].id
+  cidr_block = element(var.private_subnets,count.index)
+  availability_zone               = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) > 0 ? element(var.azs, count.index) : null
+  assign_ipv6_address_on_creation = var.assign_ipv6_address_on_creation
+  ipv6_cidr_block = var.enable_ipv6 && length(var.private_subnet_ipv6_prefixes) > 0 ? cidrsubnet(aws_vpc.myvpc[0].ipv6_cidr_block, 8, var.private_subnet_ipv6_prefixes[count.index]) : null
+  tags = {
+    Name =  "Private-Subnet-${ element(var.azs, count.index)}"
+  }
+}
+
+################################################################################
+# Internet Gateway Creation
+################################################################################
+/*
+resource "aws_internet_gateway" "MyIGW" {
+  count = var.create_vpc && var.create_igw && length(var.public_subnets) > 0 ? 1 : 0
+  vpc_id = aws_vpc.myvpc[0].id
+
+  tags = {
+    Name = "MyIGW" 
+  }
+  
+}
+
+################################################################################
+# NatGateway
+################################################################################
+
+resource "aws_eip" "MyEIP" {
+vpc = true
+tags = {
+  Name ="MyEIP" 
+  }
+}
+
+resource "aws_nat_gateway" "MyNat" {
+allocation_id = aws_eip.MyEIP.id
+subnet_id = aws_subnet.MyPublicSubnet[0].id
+tags = {
+  Name ="MyNatGateway" }
+depends_on = [aws_internet_gateway.MyIGW]
+}
+################################################################################
+# Default Route Table
+################################################################################
+resource "aws_route_table" "DefaultRT" {
+  count = var.create_vpc && length(var.public_subnets) > 0 ? 1 : 0
+
+  vpc_id = aws_vpc.myvpc[0].id
+
+  tags = {
+    Name = "DefaultRT-${ aws_vpc.myvpc[0].id}" }
+}
+################################################################################
+# Private Route Table
+################################################################################
+resource "aws_route_table" "PrivateRT" {
+  count = var.create_vpc && length(var.private_subnets) > 0 ? 1 : 0
+
+  vpc_id = aws_vpc.myvpc[0].id
+
+  tags = {
+    Name = "PrivateRT-${ aws_vpc.myvpc[0].id}" }
+}
+################################################################################
+# Adding Routes to the Route Tables
+################################################################################
+resource "aws_route" "public_internet_gateway" {
+  count = var.create_vpc && var.create_igw && length(var.public_subnets) > 0 ? 1 : 0
+
+  route_table_id         = aws_route_table.DefaultRT[0].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.MyIGW[0].id
+}
+
+resource "aws_route" "nat_gateway" {
+  route_table_id         = aws_route_table.PrivateRT[0].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_nat_gateway.MyNat.id
+}
+
+################################################################################
+# Adding Routes to the Route Tables
+################################################################################
+
+resource "aws_flow_log" "flowlogs" {
+  iam_role_arn    = aws_iam_role.vpc_flow_logs.arn
+  log_destination = aws_cloudwatch_log_group.loggroup.arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.myvpc[0].id
+}
+
+resource "aws_cloudwatch_log_group" "loggroup" {
+  name = "vpc-flow-logs"
+}
+
+resource "aws_iam_role" "vpc_flow_logs" {
+  name = "vpc_flow_logs"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "vpc-flow-logs.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "log_group_permission" {
+  name = "log_group_permission"
+  role = aws_iam_role.vpc_flow_logs.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+*/
---
+++ variables.tf
@@ -0,0 +1,131 @@
+variable "create_vpc" {
+  description = "Controls if VPC should be created (it affects almost all resources)"
+  type        = bool
+  default     = true
+}
+/* 
+Section for declaring the Variables for Creation of VPC 
+*/
+
+variable "cidr" {
+  description = "(Optional) The IPv4 CIDR block for the VPC. CIDR can be explicitly set or it can be derived from IPAM using `ipv4_netmask_length` & `ipv4_ipam_pool_id`"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "use_ipam_pool" {
+  description = "Determines whether IPAM pool is used for CIDR allocation"
+  type        = bool
+  default     = false
+}
+
+variable "ipv4_ipam_pool_id" {
+  description = "(Optional) The ID of an IPv4 IPAM pool you want to use for allocating this VPC's CIDR."
+  type        = string
+  default     = null
+}
+
+variable "ipv4_netmask_length" {
+  description = "(Optional) The netmask length of the IPv4 CIDR you want to allocate to this VPC. Requires specifying a ipv4_ipam_pool_id."
+  type        = number
+  default     = null
+}
+
+variable "enable_ipv6" {
+  description = "Requests an Amazon-provided IPv6 CIDR block with a /56 prefix length for the VPC. You cannot specify the range of IP addresses, or the size of the CIDR block."
+  type        = bool
+  default     = true
+}
+
+variable "ipv6_cidr" {
+  description = "(Optional) IPv6 CIDR block to request from an IPAM Pool. Can be set explicitly or derived from IPAM using `ipv6_netmask_length`."
+  type        = string
+  default     = null
+}
+
+variable "ipv6_ipam_pool_id" {
+  description = "(Optional) IPAM Pool ID for a IPv6 pool. Conflicts with `assign_generated_ipv6_cidr_block`."
+  type        = string
+  default     = null
+}
+
+variable "ipv6_netmask_length" {
+  description = "(Optional) Netmask length to request from IPAM Pool. Conflicts with `ipv6_cidr_block`. This can be omitted if IPAM pool as a `allocation_default_netmask_length` set. Valid values: `56`."
+  type        = number
+  default     = null
+}
+
+variable "instance_tenancy" {
+  description = "A tenancy option for instances launched into the VPC"
+  type        = string
+  default     = "default"
+}
+
+variable "enable_dns_hostnames" {
+  description = "Should be true to enable DNS hostnames in the VPC"
+  type        = bool
+  default     = false
+}
+
+variable "enable_dns_support" {
+  description = "Should be true to enable DNS support in the VPC"
+  type        = bool
+  default     = true
+}
+/* 
+Section for declaring the Variables for Creation of Public Subnet 
+*/
+variable "public_subnets" {
+  description = "A list of public subnets inside the VPC"
+  type        = list(string)
+  default     = ["10.0.0.0/24","10.0.1.0/24"]
+}
+
+variable "azs" {
+  description = "A list of availability zones names or ids in the region"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "map_public_ip_on_launch" {
+  type        = bool
+  default     = true
+}
+
+variable "assign_ipv6_address_on_creation" {
+  description = "Assign IPv6 address on subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch"
+  type        = bool
+  default     = true
+}
+
+variable "public_subnet_ipv6_prefixes" {
+  description = "Assigns IPv6 public subnet id based on the Amazon provided /56 prefix base 10 integer (0-256). Must be of equal length to the corresponding IPv4 subnet list"
+  type        = list(string)
+  default     = ["00", "01"]
+}
+/* 
+Section for declaring the Variables for Creation of Private Subnet 
+*/
+
+variable "private_subnets" {
+  description = "A list of private subnets inside the VPC"
+  type        = list(string)
+  default     = ["10.0.2.0/24","10.0.3.0/24"]
+}
+
+variable "private_subnet_ipv6_prefixes" {
+  description = "Assigns IPv6 public subnet id based on the Amazon provided /56 prefix base 10 integer (0-256). Must be of equal length to the corresponding IPv4 subnet list"
+  type        = list(string)
+  default     = ["02", "03"]
+}
+
+/* 
+Section for declaring the Variables for Creation of Private Subnet 
+*/
+
+variable "create_igw" {
+  description = "Controls if an Internet Gateway is created for public subnets and the related routes that connect them."
+  type        = bool
+  default     = true
+}
+
