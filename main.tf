da834fccdb2084d2aec772ae3ff2d8e2d9e58e29
Amit Agarwal <1989amit@gmail.com>
2022-12-13 13:04:41 +0530

    r

--- main.tf
+++ main.tf
@@ -295,7 +295,6 @@ resource "aws_ec2_transit_gateway" "network-transit" {
 }
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "transit-attachment" {
-  for_each = data.aws_subnet_ids.subnet_ids.ids
   subnet_ids         = [aws_subnet.MyPublicSubnet[0].id,aws_subnet.MyPublicSubnet[1].id]
   transit_gateway_id = aws_ec2_transit_gateway.network-transit.id
   vpc_id             = aws_vpc.myvpc[0].id
