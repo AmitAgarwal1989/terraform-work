ad66565ec36757e49de555b5131668f3bad556c9
Amit Agarwal <1989amit@gmail.com>
2022-12-13 12:47:05 +0530

    Added Data Block

--- main.tf
+++ main.tf
@@ -293,9 +293,16 @@ resource "aws_ec2_transit_gateway" "network-transit" {
     Name = "Network-Prod-E1-TG001"
   }
 }
-
+data "aws_subnet_ids" "subnet_ids"{
+  vpc_id = aws_vpc.myvpc[0].id
+  filter {
+    name = "tag:Name"
+    values = ["Network-Prod-E1-Public"]
+  }
+}
 resource "aws_ec2_transit_gateway_vpc_attachment" "transit-attachment" {
-  subnet_ids         = flatten(aws_subnet.MyPublicSubnet[0].id ,aws_subnet.MyPublicSubnet[0].id)
+  for_each = data.aws_subnet_ids.subnet_ids.ids
+  subnet_ids         = [each.value]
   transit_gateway_id = aws_ec2_transit_gateway.network-transit.id
   vpc_id             = aws_vpc.myvpc[0].id
   ipv6_support       = "enable"
